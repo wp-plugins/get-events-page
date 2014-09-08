2a932f528bcd5259282a02e9d52a5822d249af4f
GetEvents <GetEvents@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-08 09:19:24 +0000

    Google TagManager added for install tracking...
    git-svn-id: https://plugins.svn.wordpress.org/get-events-page/trunk@984134 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- getevents_admin.php
+++ getevents_admin.php
@@ -75,6 +75,14 @@ function getevents_admin_options() {
     include 'getevents_admin_options.php';
 }
 
-
-
 ?>
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-P6W3XQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P6W3XQ');</script>
+<!-- End Google Tag Manager -->
